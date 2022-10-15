5cc25b94a93bbe8184ed27686607a50662ec1dfd
SavanJavia <savanjavia@outlook.com>
2022-10-15 13:04:40 +0530

    CREATE A TRY CATCH FINALLY PROGRAM

---
+++ src/TryCatchFinally.java
@@ -0,0 +1,29 @@
+public class TryCatchFinally {
+
+    public static void main(String[] args) {
+       String str = callSubMethod();
+       System.out.println(str);
+    }
+
+    /**
+     * In Both Case It's Printed Finally Block Return Statement
+     * @return
+     */
+    private static String callSubMethod() {
+
+        try{
+            //Negative Scenario:
+            int i = 1/0;
+            return "t";
+        }
+        catch(Exception e){
+
+            return "c";
+        }
+        finally{
+
+            return "f";
+        }
+        //return "main"; // Not Worked - Compiler Time Error
+    }
+}
