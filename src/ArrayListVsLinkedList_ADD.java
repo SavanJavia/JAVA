0cfaf3f03f027050139508f1ccf51023653eb8da
SavanJavia <savanjavia@outlook.com>
2022-10-15 19:04:34 +0530

    CREATE AN ARRAYLIST VS LINKEDLIST PROGRAM

--- src/ArrayListVsLinkedList_ADD.java
+++ src/ArrayListVsLinkedList_ADD.java
@@ -12,11 +12,31 @@ public class ArrayListVsLinkedList_ADD {
 
         //ArrayList
         long startTime1 = System.nanoTime();
-        ArrayList al = new ArrayList<>();
 
-        for(int i=1; i<= 1000000; i++){
-            al.add(i);
+        ArrayList<String> al=new ArrayList<>();
+
+        for(int i=1; i<= 100000; i++) {
+            al.add("Ravi");
+            al.add("Vijay");
+            al.add("Ajay");
+            al.add("Anuj");
+            al.add("Gaurav");
+            al.add("Harsh");
+            al.add("Virat");
+            al.add("Gaurav");
+            al.add("Harsh");
+            al.add("Amit");
         }
+            al.add("Ravia");
+            al.add("Vijaya");
+            al.add("Ajaya");
+            al.add("Anuja");
+            al.add("Gaurava");
+            al.add("Harsha");
+            al.add("Virata");
+            al.add("Gaurava");
+            al.add("Harsha");
+            al.add("Amita");
         long elapsedTime1 = System.nanoTime() - startTime1;
         System.out.println("ArrayList>>>"+elapsedTime1);
 
@@ -24,11 +44,29 @@ public class ArrayListVsLinkedList_ADD {
 
         long startTime2 = System.nanoTime();
 
-        LinkedList ll = new LinkedList<>();
-
-        for(int j=1; j<= 1000000; j++){
-            ll.add(j);
+        LinkedList<String> ll=new LinkedList<>();
+        for(int i=1; i<= 100000; i++) {
+            ll.add("Ravi");
+            ll.add("Vijay");
+            ll.add("Ajay");
+            ll.add("Anuj");
+            ll.add("Gaurav");
+            ll.add("Harsh");
+            ll.add("Virat");
+            ll.add("Gaurav");
+            ll.add("Harsh");
+            ll.add("Amit");
         }
+            ll.add("Ravia");
+            ll.add("Vijaya");
+            ll.add("Ajaya");
+            ll.add("Anuja");
+            ll.add("Gaurava");
+            ll.add("Harsha");
+            ll.add("Virata");
+            ll.add("Gaurava");
+            ll.add("Harsha");
+            ll.add("Amita");
 
         long elapsedTime2 = System.nanoTime() - startTime2;
         System.out.println("LinkedList>>"+elapsedTime2);
