f9a35aac14bee87d1e1278631253701e170dfeb2
SavanJavia <savanjavia@outlook.com>
2022-10-15 14:03:01 +0530

    CREATE A CUSTOM ADD INTO ARRAYLIST PROGRAM

---
+++ src/CustomAddInToArrayList.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+/**
+ * IF YOU NEED ANY SPECIFIC VALUE ADD IN LAST USING ARRAYLIST
+ */
+public class CustomAddInToArrayList {
+
+    public static void main(String[] args) {
+
+        ArrayList al = new ArrayList<>();
+        for(char i='a';i<='z';i++) {
+            al.add(i);
+        }
+
+        ArrayList a1 = new ArrayList();
+        ArrayList a2 = new ArrayList();
+        ArrayList aFinal = new ArrayList();
+
+        for(Object a : al) {
+            if(!a.equals('o')) {
+                a1.add(a);
+            }
+            else {
+                a2.add(a);
+            }
+        }
+        aFinal.addAll(al);
+        aFinal.addAll(a2);
+
+        System.out.println(aFinal);
+    }
+}
