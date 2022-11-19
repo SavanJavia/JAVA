a7926778bd062bab8e65f18f99202cf926534e78
SavanJavia <savanjavia@outlook.com>
2022-11-19 15:42:03 +0530

    java Program

---
+++ src/ClassTest.java
@@ -0,0 +1,23 @@
+public class ClassTest {
+    public static void main(String[] args) {
+
+        String str = "this is a java test";
+        //output: test java a is this
+
+
+        String words[] = str.split("\\s");
+        String reversedString = "";
+
+        //Reverse each word's position
+        for (int i = 0; i < words.length; i++) {
+            if (i == words.length - 1)
+                reversedString = words[i] + reversedString;
+            else
+                reversedString = " " + words[i] + reversedString;
+        }
+
+        // Displaying the string after reverse
+        System.out.print("Reversed string : " + reversedString);
+    }
+
+}
