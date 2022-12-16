f07433bfed37ef68f85ce8820d005fdefe121957
SavanJavia <savanjavia@outlook.com>
2022-12-16 10:31:39 +0530

    CORE JAVA

---
+++ src/Core_Java/HashCode_Equal.java
@@ -0,0 +1,56 @@
+package Core_Java;
+
+import java.util.Objects;
+
+public class HashCode_Equal {
+    public static void main(String[] args) {
+
+        xyz x = new xyz();
+        x.setId("1");
+        System.out.println(x.hashCode());
+        xyz x1 = new xyz();
+        x1.setId("1");
+        System.out.println(x1.hashCode());
+        System.out.println(x.equals(x1));
+
+
+    }
+}
+
+
+class xyz{
+
+    String name;
+
+    String id;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+   /* @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        xyz xyz = (xyz) o;
+        return Objects.equals(name, xyz.name) && Objects.equals(id, xyz.id);
+    }*/
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, id);
+    }
+}
---
+++ src/Core_Java/StringClass.java
@@ -0,0 +1,15 @@
+package Core_Java;
+
+public class StringClass {
+    public static void main(String[] args) {
+
+        String s = "first";
+        String s1 = "second";
+        System.out.println(s+s1);
+        s = "first1";
+        System.out.println(s+s1);
+         s="Sachin";
+         s = s.concat(" Tendulkar");
+        System.out.println(s+s1);
+    }
+}
