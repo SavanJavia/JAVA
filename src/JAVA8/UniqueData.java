59fdc2b64e5a0eb168c363b8e289e2bc4f1f3832
SavanJavia <savanjavia@outlook.com>
2023-01-05 22:59:02 +0530

    CORE JAVA

--- src/JAVA8/UniqueData.java
+++ src/JAVA8/UniqueData.java
@@ -1,33 +1,20 @@
+package JAVA8;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class UniqueData {
     public static void main(String[] args) {
 
-
        /* Merge two arrays into one with only Unique numbers been printed. The output should be in ascending order.*/
 
                 List<Integer> l1 = Arrays.asList(1, 2, 22, 48, 87,3, 4, 5);
-
                 List<Integer> l2 = Arrays.asList(5, 6, 7, 48, 22);
 
-                Set<Integer> output = new HashSet<>();
+                List<Integer> list = new ArrayList<>();
+                list.addAll(l1);
+                list.addAll(l2);
 
-        for(int i =0;i<l1.size();i++){
-
-            output.add(l1.get(i));
-
-        }
-        for(int i =0;i<l2.size();i++){
-
-            output.add(l2.get(i));
-
-        }
-
-        System.out.println(output.stream().sorted().collect(Collectors.toList()));
-
-
-
-
+                System.out.println(list.stream().sorted().distinct().collect(Collectors.toList()));
     }
 }
